80cbdec3c9f22c7e4dd8c24e4797a32744dce786
unknown <jatric@inbox.ru>
2015-06-25 19:41:42 +0300

    add classes weapon change some old classes

--- GameVer0.0.2/source.cpp
+++ GameVer0.0.2/source.cpp
@@ -3,30 +3,75 @@
 
 using namespace std;
 
+
+//Classes////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Class User
 class User
 {
-	int mana = 100;
-	int strong = 5 + level;
-	int intelligence = 5 + level;
-	int agility = 5 + level;
+	int mana = 100 + Weapon.mana + Boots.mana + Head.mana + Glows.mana + Body.mana;
+	int strong = 5 + level + Weapon.strong + Boots.strong + Head.strong + Glows.strong + Body.strong;
+	int intelligence = 5 + level + Weapon.intelligence + Boots.intelligence + Head.intelligence + Glows.intelligence + Body.intelligence;
+	int agility = 5 + level + Weapon.agility + Boots.agility + Head.agility + Glows.agility + Body.agility;
 	
 
 public:
 	char * name = "bot";//get from file
 	int level = 5;//get from file, change with time or killing bots and users
-	int health = 35 + (level * 2) + Armor.head + Armor.boots + Armor.glows + Armor.body;
-	int damage = 10 + weapon + ;
-	int armor = 2;
-	int weapon = 5;
+	int health = 35 + (level * 2) + ;
+	int damage = 10 + Weapon.damage;
 
-	friend Armor;
 };
 
-//Class armor, get from file, drop from bots, or enemy castles, or can be create in your castle
+//Class Head, get from file, drop from bots, or enemy castles, or can be create in your castle
+Class Weapon
+{
+	char name = {"Name"}
+	int mana = 10;
+	int strong = 2;
+	int intelligence = 2;
+	int agility = 2;
+	int damage = 15;
+
+	friend User;
+}
+
+//Class Head, get from file, drop from bots, or enemy castles, or can be create in your castle
 Class Head
 {
-	
+	int mana = 5;
+	int strong = 2;
+	int intelligence = 1;
+	int agility = 4;
+	friend User;
+}
+
+//Class Glows, get from file, drop from bots, or enemy castles, or can be create in your castle
+Class Glows
+{
+	int mana = 5;
+	int strong = 2;
+	int intelligence = 1;
+	int agility = 4;
+	friend User;
+}	
+
+//Class Body, get from file, drop from bots, or enemy castles, or can be create in your castle
+Class Body
+{
+	int mana = 5;
+	int strong = 5;
+	int intelligence = 2;
+	int agility = 2;
+	friend User;
+}
+
+//Class Body, get from file, drop from bots, or enemy castles, or can be create in your castle
+Class Boots
+{
+	int mana = 0;
+	int strong = 2;
+	int intelligence = 1;
+	int agility = 1;
 	friend User;
 }	
 
@@ -79,6 +124,7 @@ void fight(User & u, Bot & b)
 	}
 }
 
+//END Classes /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
